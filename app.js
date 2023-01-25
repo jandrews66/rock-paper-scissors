fb8670b090e05257261974a4185e56b58d34ccff
Joe Andrews <joeandrews92@gmail.com>
2023-01-25 14:50:54 -0800

    Create game function. Use for loop to play 5 rounds in a game. Display winner in the console at the end of the 5 rounds.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let userScore = 0;
+
 function getUserChoice() { 
     userChoice = prompt("Enter rock, paper or scissors").toLowerCase();
 
@@ -29,13 +32,31 @@ function playRound() {
     
    if (computerChoice === "rock" && userChoice === "scissors" || computerChoice === "scissors" && userChoice === "paper" || computerChoice === "paper" && userChoice === "rock") {
         console.log("Computer wins");
+        computerScore += 1;
+        
     } else if (computerChoice === userChoice) {
         console.log("Draw");
     } else {
         console.log("You win");
+        userScore += 1;
     }
 
    console.log(computerChoice + " " + userChoice);
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+        console.log("Round " + (i+1) + " - Computer: " + computerScore + " vs User: " + userScore);
+     }
+    console.log("Game over! Final Score - Computer: " + computerScore + " vs User: " + userScore);
+    
+    if (computerScore > userScore) {
+        console.log("Computer wins the game!");
+    } else if (userScore > computerScore) {
+        console.log("User wins the game!");
+    } else {
+        console.log("The game is a tie");
+    }
+}
 
